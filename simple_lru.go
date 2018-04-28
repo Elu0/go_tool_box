508394215236af1610dc912e4b1b9a0c2f60316a
QianBo <bo.qian@meelive.cn>
2018-04-28 17:07:48 +0800

    add concurrency logic for LRU cache

--- simple_lru.go
+++ simple_lru.go
@@ -1,8 +1,11 @@
 package util
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"sync/atomic"
+	"unsafe"
 )
 
 type Node struct {
@@ -19,71 +22,130 @@ type LRUCache struct {
 	MapContent map[string]*Node
 }
 
-func (cache *LRUCache) Init(capacity int) {
-	cache.Cap = capacity
-	cache.Head = &Node{Key: nil, Value: nil}
-	cache.Tail = &Node{Key: nil, Value: nil}
-	cache.Head.Next = cache.Tail
-	cache.Tail.Prev = cache.Head
-	cache.MapContent = make(map[string]*Node)
+type LRUCacheAddress *LRUCache
+
+func Get(cache *LRUCacheAddress, key interface{}) (interface{}, error) {
+	newCache := LRUCache{}
+	err := DeepCopy(*cache, &newCache)
+	if err != nil {
+		return nil, err
+	}
+
+	retValue := newCache.Get(key)
+
+	for {
+		if atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(cache)),
+			unsafe.Pointer(*cache), unsafe.Pointer(&newCache)) {
+			break
+		}
+	}
+	return retValue, nil
 }
 
-func (cache *LRUCache) Get(key interface{}) (value interface{}) {
-	keyStr := cache.formatKeyToStr(key)
-	node, ok := cache.MapContent[keyStr]
-	if !ok {
-		value = nil
-		return
+func Set(cache *LRUCacheAddress, key, value interface{}) error {
+	newCache := LRUCache{}
+	err := DeepCopy(*cache, &newCache)
+	if err != nil {
+		return err
 	}
-	value = node.Value
+
+	newCache.Set(key, value)
+
+	for {
+		if atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(cache)),
+			unsafe.Pointer(*cache), unsafe.Pointer(&newCache)) {
+			break
+		}
+	}
+	return nil
+}
+
+func DeepCopy(src, dst *LRUCache) error {
+	if src == nil {
+		err := errors.New("src cannot be empty")
+		return err
+	}
+	if dst == nil {
+		err := errors.New("dst cannot be empty")
+		return err
+	}
+	dst.Init(src.Cap)
+	pd := dst.Head
+	for p := src.Head.Next; p != nil; p = p.Next {
+		if p == nil {
+			err := errors.New("node ptr cannot be empty")
+			return err
+		}
+		dNode := Node{Key: p.Key, Value: p.Value}
+		keyStr := formatKeyToStr(p.Key)
+		dst.MapContent[keyStr] = &dNode
+		pd.Next = &dNode
+		dNode.Prev = pd
+		pd = pd.Next
+	}
+	dst.Tail = pd
+	return nil
+}
+
+func (listMap *LRUCache) Init(capacity int) {
+	listMap.Cap = capacity
+	listMap.Head = &Node{Key: nil, Value: nil}
+	listMap.Tail = listMap.Head
+	listMap.MapContent = make(map[string]*Node)
+}
+
+func (listMap *LRUCache) Get(key interface{}) interface{} {
+	keyStr := formatKeyToStr(key)
+	node, ok := listMap.MapContent[keyStr]
+	if !ok {
+		return nil
+	}
 	node.Prev.Next = node.Next
 	node.Next.Prev = node.Prev
-	cache.appendTail(node)
-	return
+	listMap.appendTail(node)
+	return node.Value
 }
 
-func (cache *LRUCache) Set(key interface{}, value interface{}) {
-	keyStr := cache.formatKeyToStr(key)
-	existsNode, ok := cache.MapContent[keyStr]
+func (listMap *LRUCache) Set(key interface{}, value interface{}) {
+	keyStr := formatKeyToStr(key)
+	existsNode, ok := listMap.MapContent[keyStr]
 	if ok {
-		cache.MapContent[keyStr].Value = value
+		existsNode.Value = value
 		existsNode.Prev.Next = existsNode.Next
 		existsNode.Next.Prev = existsNode.Prev
-		cache.appendTail(existsNode)
+		listMap.appendTail(existsNode)
 		return
 	}
-	if len(cache.MapContent) == cache.Cap {
-		delNode := cache.Head.Next
-		cache.Head.Next = cache.Head.Next.Next
-		cache.Head.Next.Prev = cache.Head
-		delKeyStr := cache.formatKeyToStr(delNode.Key)
-		delete(cache.MapContent, delKeyStr)
+	if len(listMap.MapContent) == listMap.Cap {
+		delNode := listMap.Head.Next
+		listMap.Head.Next = listMap.Head.Next.Next
+		listMap.Head.Next.Prev = listMap.Head
+		delKeyStr := formatKeyToStr(delNode.Key)
+		delete(listMap.MapContent, delKeyStr)
 	}
 	newNode := &Node{Key: key, Value: value}
-	cache.appendTail(newNode)
-	cache.MapContent[keyStr] = newNode
+	listMap.appendTail(newNode)
+	listMap.MapContent[keyStr] = newNode
 }
 
-func (cache *LRUCache) appendTail(node *Node) {
-	node.Next = cache.Tail
-	node.Prev = cache.Tail.Prev
-	cache.Tail.Prev.Next = node
-	cache.Tail.Prev = node
+func (listMap *LRUCache) appendTail(node *Node) {
+	node.Next = nil
+	node.Prev = listMap.Tail
+	listMap.Tail.Next = node
+	listMap.Tail = node
 }
 
-func (cache *LRUCache) formatKeyToStr(key interface{}) (keyStr string) {
-	keyStr = ""
+func formatKeyToStr(key interface{}) string {
 	switch k := key.(type) {
 	case int64:
-		keyStr = strconv.FormatInt(k, 10)
+		return strconv.FormatInt(k, 10)
 	case int:
-		keyStr = strconv.Itoa(k)
+		return strconv.Itoa(k)
 	case float64:
-		keyStr = strconv.FormatFloat(k, 'f', -1, 64)
+		return strconv.FormatFloat(k, 'f', -1, 64)
 	case string:
-		keyStr = k
+		return k
 	default:
-		keyStr = fmt.Sprint(k)
+		return fmt.Sprint(k)
 	}
-	return
 }
